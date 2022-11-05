6270c38ec30bff003ea2d2f4b116e09f2a3e7c45
Salah Adawi <salah.adawi@outlook.com>
2022-11-05 16:33:09 +0200

    add buttons

--- src/App.js
+++ src/App.js
@@ -1,7 +1,30 @@
+import { Button, VStack, Box, Heading } from "@chakra-ui/react";
+
 function App() {
   return (
     <div className="App">
-      <h1>Hello Katya</h1>
+      <VStack mt="67px" spacing="30px">
+        <Box bg="#D9D9D9" w="256px" h="283px"></Box>
+        <Heading>NUOTIO</Heading>
+        <Button
+          colorScheme={"teal"}
+          size="lg"
+          borderRadius="20"
+          w="223px"
+          h="48px"
+        >
+          Sign in
+        </Button>
+        <Button
+          colorScheme={"teal"}
+          size="lg"
+          borderRadius="20"
+          w="223px"
+          h="48px"
+        >
+          Sign up
+        </Button>
+      </VStack>
     </div>
   );
 }
